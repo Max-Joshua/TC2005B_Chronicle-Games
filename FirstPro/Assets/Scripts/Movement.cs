cce970d42c52881933eea9463452eaa370d8be6f
IvanDLar <ivandiaz.lara.m@gmail.com>
2022-04-07 19:24:49 -0500

    Movement and walking sounds

--- FirstPro/Assets/Scripts/Movement.cs
+++ FirstPro/Assets/Scripts/Movement.cs
@@ -4,25 +4,36 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    //Player Sounds
+    [SerializeField] float delay;
+
+    //When is the next hit going to happen
+    float nextStep;
+
+    private AudioSource audioSource;
+    public AudioClip[] walkingSounds;
+    private AudioClip walkingClip;
+
+    [SerializeField] AudioClip jumping;
+    [SerializeField] AudioSource walking;
+    bool isPlaying = false;
+    bool isWalking;
+
     //Movement
     public float speed;
     public float jump;
     float moveVelocity;
     private Animator animator;
     public bool Grounded;
-    
 
-    //Grounded Vars
-    bool grounded = true;
-    
     
     void Start(){
         animator = GetComponent<Animator>();
+        audioSource = gameObject.GetComponent<AudioSource>();
+        nextStep = delay;
     }
     void Update () 
     {
-
-
 
 
         //Jumping
@@ -32,7 +43,15 @@ public class Movement : MonoBehaviour
             {
                 GetComponent<Rigidbody2D> ().velocity = new Vector2 (GetComponent<Rigidbody2D> ().velocity.x, jump);
                 animator.SetBool("isJumping", true);
-                
+
+            audioSource.clip = jumping;
+
+            if(isPlaying){
+                audioSource.Stop();
+            }else{
+                audioSource.Play();
+            }
+                isPlaying = false;
             }
         }else{
 
@@ -63,7 +82,35 @@ public class Movement : MonoBehaviour
 
         GetComponent<Rigidbody2D> ().velocity = new Vector2 (moveVelocity, GetComponent<Rigidbody2D> ().velocity.y);
 
-    }
+        if(Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.A)){
+            isWalking = true;
+        }
+
+        if(Input.GetKeyUp(KeyCode.D)){
+            isWalking = false;
+        }
+        if(Input.GetKeyUp(KeyCode.A)){
+            isWalking = false;
+        }
+
+        //Timer for each step sound
+        nextStep -= Time.deltaTime;
+
+       if(nextStep <= 0){ 
+           nextStep = delay;
+
+        if(isWalking && !walking.isPlaying && Grounded){
+            int index = Random.Range(0, walkingSounds.Length);
+            walkingClip = walkingSounds[index];
+            audioSource.clip = walkingClip;
+            audioSource.Play();
+        }
+
+        if(isWalking == false && Grounded){
+            nextStep = 0;
+        }
+            }
+        }
     //Check if Grounded
         private void OnCollisionEnter2D(Collision2D other) {
         if (other.gameObject.tag == "ground")
