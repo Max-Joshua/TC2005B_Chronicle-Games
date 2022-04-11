a09aafadb6536ca158e66ac96203c4aaaaae140e
IvanDLar <ivandiaz.lara.m@gmail.com>
2022-04-11 17:55:37 -0500

    AI Movement

--- FirstPro/Assets/Scripts/AiPatrol.cs
+++ FirstPro/Assets/Scripts/AiPatrol.cs
@@ -4,5 +4,47 @@ using UnityEngine;
 
 public class AiPatrol : MonoBehaviour
 {
+    public float walkSpeed;
 
+    [HideInInspector]
+    public bool mustPatrol;
+    public bool mustTurn;
+
+    public Rigidbody2D rb;
+    public Transform groundCheckPosition;
+    public LayerMask groundLayer;
+    public Collider2D bodyCollider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mustPatrol = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(mustPatrol){
+            Patrol();
+        }
+    }
+
+    private void FixedUpdate() {
+        if(mustPatrol){
+            mustTurn = !Physics2D.OverlapCircle(groundCheckPosition.position, 0.1f, groundLayer);
+        }
+    }
+
+    void Patrol(){
+        if(mustTurn || bodyCollider.IsTouchingLayers(groundLayer)){
+            Flip();
+        }
+        rb.velocity = new Vector2(walkSpeed * Time.fixedDeltaTime, rb.velocity.y);
+    }
+
+    void Flip(){
+        mustPatrol = false;
+        gameObject.transform.localScale = new Vector2(transform.localScale.x * -1, transform.localScale.y);
+        walkSpeed *= -1;
+        mustPatrol = true;
+    }
 }
