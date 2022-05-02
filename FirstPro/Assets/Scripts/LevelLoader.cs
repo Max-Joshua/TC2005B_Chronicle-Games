94bcdf3d7066f7a1eca757dbf6287f1e9d4aeb63
Emilio Sibaja <A01025139@itesm.mx>
2022-05-01 21:27:08 -0500

    Register Menu Pt. 1

--- FirstPro/Assets/Scripts/LevelLoader.cs
+++ FirstPro/Assets/Scripts/LevelLoader.cs
@@ -7,61 +7,6 @@ using UnityEngine.SceneManagement;
 public class LevelLoader : MonoBehaviour
 {
 
-
-
-
-
-// // Animation and play button
-//    public Animator transition;
-
-//     public float transitionTime;
-
-//     public Button startButton;
-
-
-//      public bool click = true;
-
-
-//     void Start()
-//      {
-//         startButton.onClick.AddListener(clicked);
-
-
-//     }
-
-//     void Update()
-//     {
- 
-
-//         if (click)
-//         {
-//             LoadNextLevel();
-//         }
-
-
-//     }
-
-//     void clicked()
-//     {
-//         click = true;
-//     }
-     
-
-//     public void LoadNextLevel()
-//     {
-//         StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
-//     }
-
-
-//     IEnumerator LoadLevel (int levelIndex)
-//     {
-//         transition.SetTrigger("Start");
-
-//         yield return new WaitForSeconds(transitionTime);
-
-//         SceneManager.LoadScene(levelIndex);
-//     }
-
 // Input Fields
     public string userName;
     public string age;
@@ -103,11 +48,21 @@ public class LevelLoader : MonoBehaviour
 
     void Update()
     {
-        CheckInfo();
 
-        if (click && canPlay)
+        if (click)
         {
-            PlayGame();
+            CheckInfo();
+
+            if (!canPlay)
+            {
+                click = false;
+            }
+
+            else
+            {
+                PlayGame();
+            }
+
         }
 
 
@@ -149,26 +104,21 @@ public class LevelLoader : MonoBehaviour
 
         country = countryField.GetComponent<Text>().text;
 
-        ageInt = int.Parse(age);
+        
 
     }
 
     public void CheckInfo()
     {
 
-        // if((!string.IsNullOrEmpty(userName) && userName.Length < 45) && (!string.IsNullOrEmpty(age) && (int.Parse(age) > 1 && (int.Parse(age) < 100)) && (!string.IsNullOrEmpty(eMail) && eMail.Length < 45) && (!string.IsNullOrEmpty(country) && country.Length < 45)))
-        // {
-        //     Debug.Log("A jugarle crack");
-
-        //     canPlay = true;
-        // }
-
-        if(!string.IsNullOrEmpty(userName))
+        if (!string.IsNullOrEmpty(userName) && (!string.IsNullOrEmpty(age) && (int.Parse(age) > 1 && (int.Parse(age) < 100)) && !string.IsNullOrEmpty(eMail)  && !string.IsNullOrEmpty(country) ))
         {
-            Debug.Log("A jugarle crack");
+            Debug.Log("A jugarle");
 
             canPlay = true;
         }
+
+
 
     
     }
