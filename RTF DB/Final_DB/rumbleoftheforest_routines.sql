b7be2394577900c41cafae79b559e0ef66f7cc9d
Max-Joshua <josrubcam@hotmail.com>
2022-05-04 13:32:22 -0500

    limit 10 elements

--- RTF DB/Final_DB/rumbleoftheforest_routines.sql
+++ RTF DB/Final_DB/rumbleoftheforest_routines.sql
@@ -18,20 +18,6 @@ USE `rumbleoftheforest`;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Temporary view structure for view `damage_compare`
---
-
-DROP TABLE IF EXISTS `damage_compare`;
-/*!50001 DROP VIEW IF EXISTS `damage_compare`*/;
-SET @saved_cs_client     = @@character_set_client;
-/*!50503 SET character_set_client = utf8mb4 */;
-/*!50001 CREATE VIEW `damage_compare` AS SELECT 
- 1 AS `name`,
- 1 AS `damage_taken`,
- 1 AS `damage_inflicted`*/;
-SET character_set_client = @saved_cs_client;
-
---
 -- Temporary view structure for view `top_high_scores`
 --
 
@@ -45,6 +31,19 @@ SET @saved_cs_client     = @@character_set_client;
  1 AS `age`,
  1 AS `accuracy`,
  1 AS `game_time`*/;
+SET character_set_client = @saved_cs_client;
+
+--
+-- Temporary view structure for view `top_ages`
+--
+
+DROP TABLE IF EXISTS `top_ages`;
+/*!50001 DROP VIEW IF EXISTS `top_ages`*/;
+SET @saved_cs_client     = @@character_set_client;
+/*!50503 SET character_set_client = utf8mb4 */;
+/*!50001 CREATE VIEW `top_ages` AS SELECT 
+ 1 AS `age`,
+ 1 AS `num_age`*/;
 SET character_set_client = @saved_cs_client;
 
 --
@@ -65,35 +64,18 @@ SET @saved_cs_client     = @@character_set_client;
 SET character_set_client = @saved_cs_client;
 
 --
--- Temporary view structure for view `top_ages`
+-- Temporary view structure for view `damage_compare`
 --
 
-DROP TABLE IF EXISTS `top_ages`;
-/*!50001 DROP VIEW IF EXISTS `top_ages`*/;
+DROP TABLE IF EXISTS `damage_compare`;
+/*!50001 DROP VIEW IF EXISTS `damage_compare`*/;
 SET @saved_cs_client     = @@character_set_client;
 /*!50503 SET character_set_client = utf8mb4 */;
-/*!50001 CREATE VIEW `top_ages` AS SELECT 
- 1 AS `age`,
- 1 AS `num_age`*/;
+/*!50001 CREATE VIEW `damage_compare` AS SELECT 
+ 1 AS `name`,
+ 1 AS `damage_taken`,
+ 1 AS `damage_inflicted`*/;
 SET character_set_client = @saved_cs_client;
-
---
--- Final view structure for view `damage_compare`
---
-
-/*!50001 DROP VIEW IF EXISTS `damage_compare`*/;
-/*!50001 SET @saved_cs_client          = @@character_set_client */;
-/*!50001 SET @saved_cs_results         = @@character_set_results */;
-/*!50001 SET @saved_col_connection     = @@collation_connection */;
-/*!50001 SET character_set_client      = utf8mb4 */;
-/*!50001 SET character_set_results     = utf8mb4 */;
-/*!50001 SET collation_connection      = utf8mb4_0900_ai_ci */;
-/*!50001 CREATE ALGORITHM=UNDEFINED */
-/*!50013 DEFINER=`root`@`localhost` SQL SECURITY DEFINER */
-/*!50001 VIEW `damage_compare` AS select `ui`.`name` AS `name`,`sc`.`damage_taken` AS `damage_taken`,`sc`.`damage_inflicted` AS `damage_inflicted` from (`score` `sc` join `user_info` `ui` on((`ui`.`id_user_info` = `sc`.`id_score`))) */;
-/*!50001 SET character_set_client      = @saved_cs_client */;
-/*!50001 SET character_set_results     = @saved_cs_results */;
-/*!50001 SET collation_connection      = @saved_col_connection */;
 
 --
 -- Final view structure for view `top_high_scores`
@@ -108,7 +90,25 @@ SET character_set_client = @saved_cs_client;
 /*!50001 SET collation_connection      = utf8mb4_0900_ai_ci */;
 /*!50001 CREATE ALGORITHM=UNDEFINED */
 /*!50013 DEFINER=`root`@`localhost` SQL SECURITY DEFINER */
-/*!50001 VIEW `top_high_scores` AS select `ui`.`name` AS `name`,`s`.`total_score` AS `total_score`,`ui`.`age` AS `age`,`st`.`accuracy` AS `accuracy`,`st`.`game_time` AS `game_time` from ((`score` `s` left join `user_info` `ui` on((`ui`.`id_user_info` = `s`.`id_score`))) left join `statistics` `st` on((`st`.`id_statistics` = `s`.`id_score`))) order by `s`.`total_score` desc */;
+/*!50001 VIEW `top_high_scores` AS select `ui`.`name` AS `name`,`s`.`total_score` AS `total_score`,`ui`.`age` AS `age`,`st`.`accuracy` AS `accuracy`,`st`.`game_time` AS `game_time` from ((`score` `s` join `user_info` `ui` on((`s`.`id_score` = `ui`.`id_user_info`))) join `statistics` `st` on((`st`.`id_statistics` = `s`.`id_score`))) order by `s`.`total_score` desc limit 10 */;
+/*!50001 SET character_set_client      = @saved_cs_client */;
+/*!50001 SET character_set_results     = @saved_cs_results */;
+/*!50001 SET collation_connection      = @saved_col_connection */;
+
+--
+-- Final view structure for view `top_ages`
+--
+
+/*!50001 DROP VIEW IF EXISTS `top_ages`*/;
+/*!50001 SET @saved_cs_client          = @@character_set_client */;
+/*!50001 SET @saved_cs_results         = @@character_set_results */;
+/*!50001 SET @saved_col_connection     = @@collation_connection */;
+/*!50001 SET character_set_client      = utf8mb4 */;
+/*!50001 SET character_set_results     = utf8mb4 */;
+/*!50001 SET collation_connection      = utf8mb4_0900_ai_ci */;
+/*!50001 CREATE ALGORITHM=UNDEFINED */
+/*!50013 DEFINER=`root`@`localhost` SQL SECURITY DEFINER */
+/*!50001 VIEW `top_ages` AS select `user_info`.`age` AS `age`,count(`user_info`.`age`) AS `num_age` from `user_info` group by `user_info`.`age` order by `num_age` desc */;
 /*!50001 SET character_set_client      = @saved_cs_client */;
 /*!50001 SET character_set_results     = @saved_cs_results */;
 /*!50001 SET collation_connection      = @saved_col_connection */;
@@ -132,10 +132,10 @@ SET character_set_client = @saved_cs_client;
 /*!50001 SET collation_connection      = @saved_col_connection */;
 
 --
--- Final view structure for view `top_ages`
+-- Final view structure for view `damage_compare`
 --
 
-/*!50001 DROP VIEW IF EXISTS `top_ages`*/;
+/*!50001 DROP VIEW IF EXISTS `damage_compare`*/;
 /*!50001 SET @saved_cs_client          = @@character_set_client */;
 /*!50001 SET @saved_cs_results         = @@character_set_results */;
 /*!50001 SET @saved_col_connection     = @@collation_connection */;
@@ -144,7 +144,7 @@ SET character_set_client = @saved_cs_client;
 /*!50001 SET collation_connection      = utf8mb4_0900_ai_ci */;
 /*!50001 CREATE ALGORITHM=UNDEFINED */
 /*!50013 DEFINER=`root`@`localhost` SQL SECURITY DEFINER */
-/*!50001 VIEW `top_ages` AS select `user_info`.`age` AS `age`,count(`user_info`.`age`) AS `num_age` from `user_info` group by `user_info`.`age` order by `num_age` desc */;
+/*!50001 VIEW `damage_compare` AS select `ui`.`name` AS `name`,`sc`.`damage_taken` AS `damage_taken`,`sc`.`damage_inflicted` AS `damage_inflicted` from (`score` `sc` join `user_info` `ui` on((`ui`.`id_user_info` = `sc`.`id_score`))) */;
 /*!50001 SET character_set_client      = @saved_cs_client */;
 /*!50001 SET character_set_results     = @saved_cs_results */;
 /*!50001 SET collation_connection      = @saved_col_connection */;
@@ -212,4 +212,4 @@ DELIMITER ;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-05-04 12:18:39
+-- Dump completed on 2022-05-04 13:32:10
--- RTF DB/Final_DB/rumbleoftheforest_statistics.sql
+++ RTF DB/Final_DB/rumbleoftheforest_statistics.sql
@@ -29,9 +29,9 @@ CREATE TABLE `statistics` (
   `accuracy` double NOT NULL,
   `game_time` int NOT NULL,
   `deaths` int NOT NULL,
-  `best_accuracy` int NOT NULL DEFAULT '0',
+  `best_accuracy` int DEFAULT '0',
   PRIMARY KEY (`id_statistics`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -40,28 +40,9 @@ CREATE TABLE `statistics` (
 
 LOCK TABLES `statistics` WRITE;
 /*!40000 ALTER TABLE `statistics` DISABLE KEYS */;
+INSERT INTO `statistics` VALUES (1,70.79261243435306,349,130,NULL),(2,77.07157073825017,94,178,NULL),(3,8.839578212435416,38,247,NULL),(4,32.85491142371262,155,19,NULL),(5,52.03960980225697,198,245,NULL),(6,61.6396190188315,391,147,NULL),(7,81.30973052885533,189,201,NULL),(8,17.21675995279086,330,206,NULL),(9,69.06420935622089,202,176,NULL),(10,29.003257724493125,104,112,NULL),(11,5.445829577964045,345,132,NULL),(12,94.16373715932605,26,54,NULL),(13,8.359017207745211,16,131,NULL),(14,29.20914611635359,409,17,NULL),(15,58.42863722569713,184,81,NULL),(16,85.03514327659443,439,163,NULL),(17,37.206051188315634,135,119,NULL),(18,31.83782615054678,164,21,NULL),(19,75.02318031879092,158,163,NULL),(20,19.273123490778122,234,25,NULL);
 /*!40000 ALTER TABLE `statistics` ENABLE KEYS */;
 UNLOCK TABLES;
-/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
-/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
-/*!50003 SET @saved_col_connection = @@collation_connection */ ;
-/*!50003 SET character_set_client  = utf8mb4 */ ;
-/*!50003 SET character_set_results = utf8mb4 */ ;
-/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
-/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
-/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_ENGINE_SUBSTITUTION' */ ;
-DELIMITER ;;
-/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER `statistics_BEFORE_INSERT` BEFORE INSERT ON `statistics` FOR EACH ROW BEGIN
-	IF NEW.accuracy > accuracy
-    THEN
-		SET NEW.best_accuracy = NEW.accuracy;
-	END if;
-END */;;
-DELIMITER ;
-/*!50003 SET sql_mode              = @saved_sql_mode */ ;
-/*!50003 SET character_set_client  = @saved_cs_client */ ;
-/*!50003 SET character_set_results = @saved_cs_results */ ;
-/*!50003 SET collation_connection  = @saved_col_connection */ ;
 /*!50003 SET @saved_cs_client      = @@character_set_client */ ;
 /*!50003 SET @saved_cs_results     = @@character_set_results */ ;
 /*!50003 SET @saved_col_connection = @@collation_connection */ ;
@@ -92,4 +73,4 @@ DELIMITER ;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-05-04 12:18:38
+-- Dump completed on 2022-05-04 13:32:09
